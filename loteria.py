2083116085a0c4a05db9b443fa900a3390bf0326
Alex Simões <lxlx19@gmail.com>
2024-12-26 21:48:57 -0300

    format docstring

--- loteria.py
+++ loteria.py
@@ -13,15 +13,19 @@ class Loteria:
     jogos2 : list
         a list to store all the generated lottery games
     precos : dict
-        a dictionary to store the price of the lottery games based on the number of chosen numbers
+        a dictionary to store the price of the lottery games based on
+        the number of chosen numbers
     Methods
     -------
     sortear_jogos(quant, num_numeros)
-        Draws a specified number of lottery games, each containing a specified number of numbers.
+        Draws a specified number of lottery games, each containing
+        a specified number of numbers.
     calcular_valor_aposta(num_numeros, quant)
-        Calculates the total bet amount based on the number of chosen numbers and the number of bets.
+        Calculates the total bet amount based on the number of
+        chosen numbers and the number of bets.
     executar()
-        Executes the lottery game by prompting the user for the number of games and the number of numbers per game.
+        Executes the lottery game by prompting the user for the
+        number of games and the number of numbers per game.
     """
 
     def __init__(self):
@@ -47,20 +51,22 @@ class Loteria:
 
     def sortear_jogos(self, quant, num_numeros):
         """
-        Sorteia uma quantidade especificada de jogos de loteria, cada um contendo um número especificado de números.
+        Draws a specified number of lottery games,
+        each containing a specified number of numbers.
 
         Args:
-            quant (int): A quantidade de jogos a serem sorteados.
-            num_numeros (int): O número de números em cada jogo.
+            quant (int): The number of games to be drawn.
+            num_numeros (int): The number of numbers in each game.
 
         Returns:
             None
 
-        This method prints the sorted lottery games and appends them to the jogos2 list.
+        This method prints the sorted lottery games and
+        appends them to the jogos2 list.
         """
         print(f'{" SORTEANDO JOGOS ":=^40}')
-        for c in range(quant):
-            for i in range(num_numeros):
+        for _ in range(quant):
+            for _ in range(num_numeros):
                 while True:
                     num = randint(1, 60)
                     if num not in self.jogos:
@@ -76,14 +82,16 @@ class Loteria:
 
     def calcular_valor_aposta(self, num_numeros, quant):
         """
-        Calcula o valor total da aposta com base no número de números escolhidos e na quantidade de apostas.
+        Calculates the total bet amount based on the number
+        of chosen numbers and the number of bets.
 
         Args:
-            num_numeros (int): O número de números escolhidos na aposta.
-            quant (int): A quantidade de apostas.
+            num_numeros (int): The number of chosen numbers in the bet.
+            quant (int): The number of bets.
 
         Returns:
-            None: Esta função imprime o valor total da aposta ou uma mensagem de erro se o número de apostas for inválido.
+            None: This function prints the total bet amount or
+            an error message if the number of bets is invalid.
         """
         if num_numeros in self.precos:
             valor_total = self.precos[num_numeros] * quant
@@ -93,25 +101,33 @@ class Loteria:
 
     def executar(self):
         """
-        Executes the lottery game by prompting the user for the number of games and the number of numbers per game.
+        Executes the lottery game by prompting the user for
+        the number of games and the number of numbers per game.
         The method performs the following steps:
         1. Prints the game header.
         2. Prompts the user to input the number of games they want to play.
-        3. Prompts the user to input the number of numbers per game (between 6 and 20).
-        4. Validates the input for the number of numbers per game and prompts again if the input is invalid.
+        3. Prompts the user to input the number of numbers
+        per game (between 6 and 20).
+        4. Validates the input for the number of numbers per
+        game and prompts again if the input is invalid.
         5. Calls the method to draw the games with the specified parameters.
-        6. Calls the method to calculate the total bet amount based on the number of numbers per game and the number of games.
+        6. Calls the method to calculate the total bet amount based
+        on the number of numbers per game and the number of games.
         """
         print("-" * 30)
         print("      JOGO DA LOTERIA")
         print("-" * 30)
 
         quant = int(input("Quantos jogos você quer fazer? "))
-        num_numeros = int(input("Você quer jogos de quantos números (6 a 20)? "))
+        num_numeros = int(
+            input("Você quer jogos de quantos números (6 a 20)? ")
+            )
 
         while num_numeros < 6 or num_numeros > 20:
-            print("Escolha inválida! Por favor, escolha um número entre 6 e 20.")
-            num_numeros = int(input("Você quer jogos de quantos números (6 a 20)? "))
+            print("Por favor, escolha um número entre 6 e 20.")
+            num_numeros = int(
+                input("Você quer jogos de quantos números (6 a 20)? ")
+                )
 
         self.sortear_jogos(quant, num_numeros)
         self.calcular_valor_aposta(num_numeros, quant)
